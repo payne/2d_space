40c9cd3c4618f502bd53bf94c7427b782fbaf161
Matt Payne <Payne@MattPayne.org>
2020-03-15 17:58:29 -0500

    Example from https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event

--- src/defender.js
+++ src/defender.js
@@ -3,12 +3,24 @@ export default class Defender {
     constructor(x,y) {
         this.x=x;
         this.y=y;
+        this.delta=0;
+
+        document.addEventListener("keydown", event => {
+            if (event.isComposing || event.keyCode === 229) {
+                return;
+            }
+            // do something
+            this.x+=10;
+        });
+    }
+
+    keyDownHandler(e) {
+       this.x-=10;
     }
 
     update(ctx) {
         ctx.fillStyle='pink';
-        ctx.fillRect(this.x,this.y,10,10);
-        this.x+=10;
         this.x%=100;
+        ctx.fillRect(this.x,this.y,10,10);
     }
 }
